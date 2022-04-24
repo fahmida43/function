1c26be5478bc22ce91627f66ada0e19b733a9a5d
fahmida43 <99779584+fahmida43@users.noreply.github.com>
2022-04-24 09:02:26 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,52 @@
+function sum(a,b,c) {
+    document.write( a + b + c + "<br>");
+}
+sum(3,2,3)
+
+function substract(a,b) {
+    document.write( a - b + "<br>");
+}
+
+substract(12,5)
+
+
+function Multiplication(a,b) {
+    document.write( a * b + "<br>");
+}
+
+Multiplication(5,5)
+
+
+function division(a,b) {
+    document.write( a / b + "<br>");
+}
+
+division(50,5)
+
+
+function Modulus(a,b) {
+    document.write( a % b + "<br>");
+}
+
+Modulus(535,4)
+
+
+
+function increment(a) {
+    document.write( ++a + "<br>");
+}
+
+increment(5)
+
+
+function decrement(a) {
+    document.write( --a + "<br>");
+}
+
+decrement(10)
+
+
+
+
+
+
